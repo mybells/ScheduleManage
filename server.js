cd10aa15538ad6b3a2d6a29202983d07a8148bd7
mybells <435541908@qq.com>
2020-02-24 15:50:01 +0800

    update server.js

--- server.js
+++ server.js
@@ -10,20 +10,20 @@ const schedule = require('./routes/schedule');
 
 /* mongoose连接 */
 /* 本地数据库 */
-const uri = "mongodb+srv://mybells:test1234@cluster0-ryeek.mongodb.net/test?retryWrites=true&ssl=true&authSource=admin";
-// const uri = "mongodb://localhost:27017/scheduledb";
-mongoose.connect(uri, { useNewUrlParser: true}).then(
-  () => { console.log("connect is success!") },
-  err => { console.log(err)}
-);
+// const uri = "mongodb+srv://mybells:test1234@cluster0-ryeek.mongodb.net/test?retryWrites=true&ssl=true&authSource=admin";
+// // const uri = "mongodb://localhost:27017/scheduledb";
+// mongoose.connect(uri, { useNewUrlParser: true}).then(
+//   () => { console.log("connect is success!") },
+//   err => { console.log(err)}
+// );
 
 // /* 远程mongodb数据库 https://cloud.mongodb.com     435541908@qq.com/435541908@qq.com*/
 // //test是数据库
-// const uri = "mongodb+srv://mybells:test1234@cluster0-ryeek.mongodb.net/test?retryWrites=true&ssl=true&authSource=admin";
-// mongoose.connect(uri, { useNewUrlParser: true}).then(
-//   () => { console.log("Mongodb Connect Sucess") },
-//   err => { console.log(err)}
-// );
+const uri = "mongodb+srv://mybells:test1234@cluster0-ryeek.mongodb.net/test?retryWrites=true&ssl=true&authSource=admin";
+mongoose.connect(uri, { useNewUrlParser: true}).then(
+  () => { console.log("Mongodb Connect Sucess") },
+  err => { console.log(err)}
+);
 
 
 
