81a61e7dc5be48e61e358ef779b9d43cfb59b822
mybells <435541908@qq.com>
2020-05-18 22:38:52 +0800

    add: 效果图

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # ScheduleManage
 
-日程管理<br/>
+## 日程管理
 
-技术栈主要使用 vue-cli3.9.2+vue+elementUI+vuex+vue-router+axios<br/>
-              node+express+mongoose
+![效果图](./client/public/GIF.gif)
+
+技术栈主要使用
+
+前端：vue-cli3.9.2+vue+elementUI+vuex+vue-router+axios
+
+后台：node+express+mongoose
 
 ## 安装 npm 包
 
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const schedule = require('./routes/schedule');
 
 /* mongoose连接 */
 /* 本地数据库 */
-// const uri = "mongodb+srv://mybells:test1234@cluster0-ryeek.mongodb.net/test?retryWrites=true&ssl=true&authSource=admin";
+// const uri = "mongodb+srv://mybells:cc1pxpgxb8Wuoy7g@cluster0-ryeek.mongodb.net/test?retryWrites=true&ssl=true&authSource=admin";
 // // const uri = "mongodb://localhost:27017/scheduledb";
 // mongoose.connect(uri, { useNewUrlParser: true}).then(
 //   () => { console.log("connect is success!") },
@@ -19,17 +19,17 @@ const schedule = require('./routes/schedule');
 
 // /* 远程mongodb数据库 https://cloud.mongodb.com     435541908@qq.com/435541908@qq.com*/
 // //test是数据库
-const uri = "mongodb+srv://mybells:test1234@cluster0-ryeek.mongodb.net/test?retryWrites=true&ssl=true&authSource=admin";
-mongoose.connect(uri, { useNewUrlParser: true}).then(
+const uri = "mongodb+srv://mybells:cc1pxpgxb8Wuoy7g@cluster0-ryeek.mongodb.net/test?retryWrites=true&ssl=true&authSource=admin";
+mongoose.connect(uri, { useNewUrlParser: true }).then(
   () => { console.log("Mongodb Connect Sucess") },
-  err => { console.log(err)}
+  err => { console.log(err) }
 );
 
 
 
 /* mongodb连接 */
 // const MongoClient = require('mongodb').MongoClient;
-// var uriTestDb = "mongodb+srv://mybells:test1234@cluster0-ryeek.mongodb.net?retryWrites=true&ssl=true&authSource=admin";
+// var uriTestDb = "mongodb+srv://mybells:cc1pxpgxb8Wuoy7g@cluster0-ryeek.mongodb.net?retryWrites=true&ssl=true&authSource=admin";
 // MongoClient.connect(uriTestDb,{ useNewUrlParser: true}, function(err, client) {
 //   if(err){
 //     console.log(err);
@@ -48,7 +48,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //设置允许跨域访问该服务.
-app.all('*', function(req, res, next) {
+app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
   res.header('Access-Control-Allow-Headers', 'Content-Type');
